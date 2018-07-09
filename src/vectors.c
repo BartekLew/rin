ed14926dcaf147b07065fa67bb9294c77fb82f07
Lew <barpas@zoho.com>
2018-07-09 14:53:59 +0200

    scale shapes

--- src/vectors.c
+++ src/vectors.c
@@ -15,30 +15,37 @@ static uint len (Mov v) {
 	return (uint)(sqrt(v.x*v.x + v.y*v.y));
 }
 
-static Mov add (Mov a, Mov b) {
-	return (Mov) {.x = a.x + b.x, .y = a.y + b.y};
-}
-
 static void touch (Context *ctx);
 static void move (Context *ctx);
 static void release (Context *ctx);
 
-Point	start, ref;
 uint	mov_len;
 #define Min_curve 100
 #define Min_mov   30
+ 
+#define	Route_max 0x20
+Point   route[Route_max];
+uint	route_len = 0;
 
 static void touch (Context *ctx) {
-	ref = start = ctx->point;
 	mov_len = 0;
+	route_len = 1;
+	route[0] = ctx->point;
 }
 
-static void print_last_mov(Context *ctx){
-	printf( "%d %d -> %d %d (%u)\n",
-			_s (start.x - ref.x), _s (start.y - ref.y),
-			_s (ctx->point.x - ref.x), _s (ctx->point.y - ref.y),
-			_u len (mov(start, ctx->point))
-	      );
+static void scale_route (Point *pts, uint len) {
+	Point copy[len];
+	loop(i, len) copy[i] = pts[i];
+
+	loop(i, len) {
+		uint x = 0, y = 0;
+
+		loop(j, len) {
+			if (copy[i].x > copy[j].x) x++;
+			if (copy[i].y > copy[j].y) y++;
+		}
+		pts[i] = (Point){.x = x, .y = y};
+	}
 }
 
 static void move (Context *ctx) {
@@ -47,17 +54,28 @@ static void move (Context *ctx) {
 		return;
 
 	mov_len += last_len;
-	uint abs_len = len (mov(start, ctx->point));
+	uint abs_len = len (mov(route[route_len-1], ctx->point));
 
 	if (abs(mov_len - abs_len) > Min_curve) {
-		print_last_mov(ctx);
-		start = ctx->point;
+		if (route_len == Route_max) {
+			Warn ("Too long route");
+			return;
+		}
+		route[route_len++] = ctx->point;
 		mov_len = 0;
 	}
 }
 
 static void release (Context *ctx) {
-	print_last_mov(ctx);
+	if (mov_len >0)
+		route[route_len++] = ctx->point;
+
+	scale_route (route, route_len);
+	loop (i, route_len) {
+		printf ("%u %u\t", _u route[i].x, _u route[i].y);
+	}
+
+	printf("\n");
 }
 
 int main (int args_count, char **args) {
